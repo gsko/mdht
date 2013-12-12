52a4fe1ef342b915c07fc9eeb57e6419c1547a85
Greg Skoczek <gsk067@gmail.com>
2013-12-11 20:57:25 -0600

    making a thin-client

--- mdht_server/app.py
+++ mdht_server/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
+from twisted import web
 from twisted.application import internet, service
-from twisted.application.internet import UDPServer
+from twisted.application.internet import UDPServer, TCPServer
 from twisted.internet.protocol import Factory, Protocol
 from twisted.python import log
 from twisted.scripts.twistd import run
+from twisted.web import xmlrpc
 
 from mdht.protocols.krpc_iterator import KRPC_Iterator
-from mdht_app import config
+from mdht_server import config
 
 APPLICATION_NAME = "mdht_server"
 
@@ -14,5 +16,15 @@ app = service.Application(APPLICATION_NAME)
 kad_proto = KRPC_Iterator()
 kad_server = UDPServer(config.SERVER_PORT, kad_proto)
 kad_server.setServiceParent(app)
+
+class RPC(xmlrpc.XMLRPC):
+    def xmlrpc_hello(self, name):
+        return 'hello %s!' % name
+
+r = RPC()
+rpc_server = TCPServer(5000, web.server.Site(r))
+rpc_server.setServiceParent(app)
+
+application = app
 
 log.msg('%s running on localhost:%d', config.SERVER_PORT)
