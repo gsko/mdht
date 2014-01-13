f3aa9cfcb118a1539d8e2e4dab6853b535d1d61c
Greg Skoczek <gsk067@gmail.com>
2014-01-12 22:42:29 -0600

    all initial get() unit tests are passing; the single put() test case is still a stub; get() itself is missing token caching, which will result in poor put() latency, though this isnt a major concern since there is currently no put() implementation (talk about premature optimization)

--- mdht/protocols/krpc_simple.py
+++ mdht/protocols/krpc_simple.py
@@ -1,19 +1,24 @@
 from mdht.protocols.krpc_iterator import KRPC_Iterator
+from mdht.protocols.errors import TimeoutError, KRPCError 
 
-class LiveResultError(Exception):
+class LiveSearchError(Exception):
     def __init__(self):
         pass
 
-class LiveResult(object):
-    def __init__(self):
+class LiveSearch(object):
+    def __init__(self, target_id):
         self.results = []
         self.listeners = set()
+        # TODO refactor is_complete into is_completed()
         self.is_complete = False
+        self.outstanding_queries = 0
+        self.target_id = target_id
+        self.queried_nodes = set()
 
     def add_results(self, results):
-        """Add an iterable of results to the LiveResult"""
+        """Add an iterable of results to the LiveSearch"""
         if self.is_complete:
-            raise LiveResultError()
+            raise LiveSearchError()
         self.results += list(results)
         self._alert_listeners()
 
@@ -24,14 +29,14 @@ class LiveResult(object):
     def register_listener(self, listener):
         """
         Register an argument-less callable object that is fired
-        whenever new results are added, or if the LiveResult has been
+        whenever new results are added, or if the LiveSearch has been
         marked completed and no more results will be inserted
         """
         self.listeners.add(listener)
 
     def mark_completed(self):
         """
-        After this LiveResult is marked complete, it will fire one last
+        After this LiveSearch is marked complete, it will fire one last
         time and no longer accept new results
         """
         self.is_complete = True
@@ -41,30 +46,59 @@ class LiveResult(object):
         for listener in self.listeners:
             listener()
 
-
 class KRPC_Simple(KRPC_Iterator):
     def __init__(self, node_id=None):
         KRPC_Iterator.__init__(self, node_id)
+        # TODO token gathering and cache
 
     def get(self, target_id):
         """
-        Return a LiveResult of (node, peers) pairs
+        Return a LiveSearch of (node, peers) pairs
         for the torrent with an infohash of `target_id`
 
         (node, peers) is the contact.Node that returned the corresponding peers
         """
-        live_result = LiveResult()
+        live_search = LiveSearch(target_id)
         search_nodes = set(self.routing_table.get_closest_nodes(target_id))
-
-    def _get_peers_response_handler(self, response, live_result):
-        if responses.peers is not None:
-            pass
-        if response.nodes is not None:
-            pass
+        if len(search_nodes) == 0:
+            live_search.mark_completed()
+        else:
+            self._get_iterate(search_nodes, live_search)
+        return live_search
 
     def put(self, target_id, port):
         """
         Register this IP along with the given port
         for the torrent with an infohash of `target_id`
         """
-        live_result = LiveResult()
+        pass
+
+    def _get_iterate(self, nodes, live_search):
+        search_nodes = nodes or []
+        for node in search_nodes:
+            if node in live_search.queried_nodes:
+                continue
+            d = self.get_peers(node.address, live_search.target_id)
+            live_search.queried_nodes.add(node)
+            # TODO refactor outstanding_queries and is_completed()
+            # into something else. maybe add some kind of 'query()' and
+            # 'ack_query()'?
+            live_search.outstanding_queries += 1
+            d.addCallback(self._get_peers_response_handler, live_search)
+            d.addErrback(self._get_peers_error_handler, live_search)
+            d.addBoth(self._check_completion, live_search)
+
+    def _check_completion(self, _ignore, live_search):
+        live_search.outstanding_queries -= 1
+        if live_search.outstanding_queries == 0:
+            live_search.mark_completed()
+
+    def _get_peers_response_handler(self, response, live_search):
+        if response.peers is not None:
+            # TODO accumuluate peers (??)
+            live_search.add_results(response.peers)
+        self._get_iterate(response.nodes, live_search)
+
+    def _get_peers_error_handler(self, failure, live_search):
+        f = failure.trap(TimeoutError, KRPCError)
+        # do nothing: we don't care about timeouts or errors
--- mdht/test/protocols/test_krpc_simple.py
+++ mdht/test/protocols/test_krpc_simple.py
@@ -3,100 +3,107 @@ from twisted.internet import defer
 
 from mdht.coding import krpc_coder
 from mdht.contact import Node
-from mdht.protocols.krpc_simple import LiveResult, LiveResultError, KRPC_Simple
-from mdht.test.utils import test_nodes, HollowTransport
+from mdht.protocols.krpc_simple import LiveSearch, LiveSearchError, KRPC_Simple
+from mdht.test.utils import test_nodes, HollowTransport, HollowReactor
 
 TEST_NODE_ID = 5
 TEST_PORT = 10
+TEST_TARGET_ID = 98931616
 
-class LiveResult_TestCase(unittest.TestCase):
+class LiveSearch_TestCase(unittest.TestCase):
     def test_add_result_IncreasesResultSize(self):
-        live_result = LiveResult()
-        results = live_result.get_results()
+        live_search = LiveSearch(TEST_TARGET_ID)
+        results = live_search.get_results()
         self.assertEquals(0, len(results))
-        live_result.add_results([5])
-        results = live_result.get_results()
+        live_search.add_results([5])
+        results = live_search.get_results()
         self.assertEquals(1, len(results))
 
     def test_add_results_noMoreResultsAcceptedAfterComplete(self):
-        live_result = LiveResult()
-        live_result.mark_completed()
-        self.assertRaises(LiveResultError, live_result.add_results, [1,2])
+        live_search = LiveSearch(TEST_TARGET_ID)
+        live_search.mark_completed()
+        self.assertRaises(LiveSearchError, live_search.add_results, [1,2])
 
     def _listener(self):
         self.call_count += 1
 
     def test_register_listener_FiresListenerOnAdd(self):
         self.call_count = 0
-        live_result = LiveResult()
-        live_result.register_listener(self._listener)
-        live_result.add_results([1,2,3,4])
+        live_search = LiveSearch(TEST_TARGET_ID)
+        live_search.register_listener(self._listener)
+        live_search.add_results([1,2,3,4])
         self.assertEquals(1, self.call_count)
 
     def test_register_listener_FiresListenerOnCompletion(self):
         self.call_count = 0
-        live_result = LiveResult()
-        live_result.register_listener(self._listener)
-        live_result.add_results([99,100])
-        live_result.mark_completed()
+        live_search = LiveSearch(TEST_TARGET_ID)
+        live_search.register_listener(self._listener)
+        live_search.add_results([99,100])
+        live_search.mark_completed()
         # one call for results,
         # one call for completion
         self.assertEquals(2, self.call_count)
 
     def test_mark_completed_SetsFlag(self):
-        live_result = LiveResult()
-        live_result.mark_completed()
-        self.assertTrue(live_result.is_complete)
+        live_search = LiveSearch(TEST_TARGET_ID)
+        live_search.mark_completed()
+        self.assertTrue(live_search.is_complete)
 
 class KRPC_Simple_TestCase(unittest.TestCase):
     def setUp(self):
         self.ksimple = KRPC_Simple(TEST_NODE_ID)
         self.ksimple.transport = HollowTransport()
-
-    def _grab_outbound_get_peers(self):
-        encoded_query = self.ksimple.transport.packet
-        self.ksimple.transport._reset()
-        self.assertNotEquals(None, encoded_query)
-        query = krpc_coder.decode(encoded_query)
-        self.assertEquals("get_peers", query.rpctype)
-        return query 
+        self.ksimple._reactor = HollowReactor()
 
     def test_get_liveResultGrowsWithSingleResponse(self):
-        self._prepare_proto()
+        seed_node = self._init_seed_node()
         
-        live_result = self.ksimple.get(890)
+        live_search = self.ksimple.get(890)
+        self.assertTrue(seed_node in live_search.queried_nodes)
         query = self._grab_outbound_get_peers()
+        result_peer = test_nodes[33].address
         response = query.build_response(peers=[result_peer])
-        self._encode_and_respond(response)
-        results = live_result.get_results()
+        responding_node = self._encode_and_respond(response)
+        results = live_search.get_results()
         self.assertEquals(1, len(results))
-        expected_result = (responding_node, result_peer)
+        self.assertTrue(live_search.queried_nodes)
         for result in results:
-            self.assertEquals(expected_result, result)
+            self.assertEquals(result_peer, result)
             break
+        self.assertTrue(live_search.is_complete)
 
     def test_get_protoSendsAnotherGetPeersQueryOnResponseWithNodes(self):
-        self._prepare_proto()
+        seed_node = self._init_seed_node()
         
-        live_result = self.ksimple.get(890)
+        live_search = self.ksimple.get(890)
+        self.assertTrue(seed_node in live_search.queried_nodes)
         first_query = self._grab_outbound_get_peers()
         # any address tuple will do as a result_peer
         result_node = test_nodes[1]
-        response = krpc.build_response(nodes=[result_node])
+        response = first_query.build_response(nodes=[result_node])
+        self.assertEquals(0, len(live_search.get_results()))
         self._encode_and_respond(response)
+        self.assertFalse(live_search.is_complete)
+        self.assertTrue(result_node in live_search.queried_nodes)
         second_query = self._grab_outbound_get_peers()
         self.assertNotEquals(second_query, first_query)
+        self.assertFalse(live_search.is_complete)
 
     def test_get_liveResultDoesntGrowWithError(self):
-        self._prepare_proto()
+        seed_node = self._init_seed_node()
 
-        live_result = self.ksimple.get(890)
-        krpc = self._grab_outbound_get_peers()
-        # any address tuple will do as a result_peer
-        error = krpc.build_error()
-        responding_node = self._encode_and_respond(error)
-        results = live_result.get_results()
+        live_search = self.ksimple.get(TEST_TARGET_ID)
+        self.assertTrue(seed_node in live_search.queried_nodes)
+        query = self._grab_outbound_get_peers()
+        error = query.build_error()
+        self._encode_and_respond(error)
+        results = live_search.get_results()
         self.assertEquals(0, len(results))
+
+    def test_get_completionWithNoPeers(self):
+        live_search = self.ksimple.get(TEST_TARGET_ID)
+        self.assertEquals(0, len(live_search.get_results()))
+        self.assertTrue(live_search.is_complete)
 
     def test_put(self):
         self.assertTrue(False)
@@ -105,12 +112,20 @@ class KRPC_Simple_TestCase(unittest.TestCase):
         krpc._from = 123
         encoded_krpc = krpc_coder.encode(krpc)
         responding_node = test_nodes[22]
-        self.ksimple.datagramReceived(
-            encoded_krpc, responding_node.node_addres)
+        self.ksimple.datagramReceived(encoded_krpc, responding_node.address)
         return responding_node
 
-    def _prepare_proto(self):
+    def _init_seed_node(self):
         # prepare proto for search
         seed_node = test_nodes[0]
         node_accepted = self.ksimple.routing_table.offer_node(seed_node)
         self.assertTrue(node_accepted)
+        return seed_node
+
+    def _grab_outbound_get_peers(self):
+        encoded_query = self.ksimple.transport.packet
+        self.ksimple.transport._reset()
+        self.assertNotEquals(None, encoded_query)
+        query = krpc_coder.decode(encoded_query)
+        self.assertEquals("get_peers", query.rpctype)
+        return query 
