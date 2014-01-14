75ddadf2906573cc2cc3ad67ed5f07eef33b0945
Greg Skoczek <gsk067@gmail.com>
2014-01-13 21:12:59 -0600

    removing old example file

--- mdht/example.py
+++
@@ -1,9 +0,0 @@
-from twisted.python import logging
-
-from mdht.run import Server
-from mdht.constants import bootstrap_addresses
-
-rand_id = 216882130869719664159264778321560871326200781259L
-dht = MDHT(rand_id)
-
-dht.run()
